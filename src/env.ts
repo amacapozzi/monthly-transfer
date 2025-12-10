940af5f55cb3f4260017e834486c66691785912a
amacapozzi <mosqueracapozzia@gmail.com>
2025-12-10 00:24:34 -0300

    Add drizzle-orm and drizzle-typebox dependencies; update error message for missing environment variables

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import { AppLogger } from "@/core/logger";
 
 export function loadEnv() {
   if (!env.DATABASE_URL) {
-    const error = new Error("Missing environment variable: DATABASE_URL");
+    const error = new Error("Missing environments variables");
     AppLogger.error(error);
     throw error;
   }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Elysia } from "elysia";
+import { loadEnv } from "@/env";
+
+loadEnv();
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
